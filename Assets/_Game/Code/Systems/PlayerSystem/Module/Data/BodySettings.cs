615eb970298afbd1b235b6c0992ce67fe34dddda
Gerard <gerardo_115@hotmail.es>
2023-11-28 10:58:31 -0500

    Player customization Create animations for player Player movement

---
+++ Assets/_Game/Code/Systems/PlayerSystem/Module/Data/BodySettings.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Game.Systems.PlayerSystem.Data
+{
+    public enum BodyParts
+    {
+        Hair,
+        Cloth,
+        Pants
+    }
+    
+    [System.Serializable]
+    public struct BodyData
+    {
+        public string name; 
+        public BodyParts BodyPart;
+        public BodyPartSettings[] BodyPartSettings;
+    }
+    
+    [CreateAssetMenu(fileName = "BodySettings", menuName = "Game/Player/Body Settings")]
+    public class BodySettings : ScriptableObject
+    {
+        [SerializeField]
+        private BodyData[] bodyData;
+
+        public BodyData[] BodyData => bodyData; 
+    }
+}
